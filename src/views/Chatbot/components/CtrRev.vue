3a0d634931a18d92236d02a692eb5f28f3fdddab
jeby <jiabiao@shouqiev.com>
2025-08-29 13:36:38 +0800

    ci: 法睿829-审核v1

--- src/views/Chatbot/components/CtrRev.vue
+++ src/views/Chatbot/components/CtrRev.vue
@@ -1,31 +1,69 @@
 <script setup lang="ts">
-import {onBeforeUnmount, reactive, ref} from 'vue'
+import type {IAIService} from "@/services/aiService.ts";
+
+const aiService = inject<IAIService>('aiService')!
+
+import {inject, onBeforeUnmount, reactive, watch} from 'vue'
 import Creator from './CtrComp/Creator.vue'
+import {message} from "ant-design-vue";
 const uploadStatus = reactive({
+  fileId: '',
   uploaded: false,
   progress: 0,
+  procText: '',
   dots: '',
   interval: null as any
 })
 
+const startProgress = (text = '分析中') => {
+  uploadStatus.procText = text
+  uploadStatus.progress = 0
+  uploadStatus.interval = setInterval(() => {
+    if (uploadStatus.progress < 90) {
+      uploadStatus.progress += Math.floor(Math.random() * 10) + 1
+    } else if (uploadStatus.progress < 100) {
+      uploadStatus.progress += 1
+    } else {
+      clearInterval(uploadStatus.interval)
+      uploadStatus.interval = null
+    }
+    let dotCount = (uploadStatus.dots.length + 1) % 4
+    uploadStatus.dots = '.'.repeat(dotCount)
+  }, 300)
+}
 const uploadChange = (file:any) => {
   uploadStatus.uploaded = true
-  uploadStatus.progress = 0
-  let dotCount = 0
-
-  uploadStatus.interval = setInterval(() => {
-    if (uploadStatus.progress < 100) {
-      dotCount = (dotCount + 1) % 4
-      uploadStatus.dots = '.'.repeat(dotCount)
-      uploadStatus.progress += 10
-    }
-  }, 500)
+  uploadStatus.fileId = file?.response?.data ?? ''
+  startProgress()
 }
+const getRules = () => {
+  if (!uploadStatus?.fileId) {
+    message.warning('文件故障,请重新上传!')
+    return
+  }
+  aiService.post('/chat/farui/contract/rule/generate',{
+    assistant: {
+      metaData: {
+        fileId: uploadStatus?.fileId,
+        position: '1'
+      }
+    }
+  })
+    .then(res => {
+
+    })
+}
+
+watch(() => uploadStatus.progress, (newVal) => {
+  if (newVal >= 100) getRules()
+})
+
 onBeforeUnmount(() => {
   if (uploadStatus.interval) {
     clearInterval(uploadStatus.interval)
   }
 })
+
 </script>
 
 <template>
